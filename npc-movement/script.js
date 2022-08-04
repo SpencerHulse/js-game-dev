39a9b807c0c260a94d67489cc4fb756a5073fd23
Spencer Hulse <hulse.spencer@gmail.com>
2022-08-04 01:56:56 -0400

    Updated Enemy class with enemy images and animation variables.

--- npc-movement/script.js
+++ npc-movement/script.js
@@ -7,32 +7,62 @@ const CANVAS_HEIGHT = (canvas.height = 1000);
 const numberOfEnemies = 100;
 const enemiesArray = [];
 
+let gameFrame = 0;
+
 class Enemy {
   constructor() {
+    this.image = new Image();
+    this.image.src = "./assets/enemy1.png";
     // The use of Math.random() has the enemies appear at random places
     this.x = Math.random() * canvas.width;
     this.y = Math.random() * canvas.height;
-    this.width = 100;
-    this.height = 100;
+    // The minus allows some to go to the left and some to the right
+    this.speed = Math.random() * 4 - 2;
+    // Sprite sheet width / frames
+    this.spriteWidth = 293;
+    this.spriteHeight = 155;
+    // Retains aspect ratio
+    this.width = this.spriteWidth / 2.5;
+    this.height = this.spriteHeight / 2.5;
+    this.frame = 0;
+    // Gets a random number between 1 and 4 for flap speed
+    this.flapSpeed = Math.floor(Math.random() * 3 + 1);
   }
   // Update coordinates for movement
   update() {
-    this.x++;
-    this.y++;
+    this.x += this.speed;
+    this.y += this.speed;
+    // Animate sprites - only every so many animation cycles by flap speed
+    if (gameFrame % this.flapSpeed === 0) {
+      this.frame > 4 ? (this.frame = 0) : this.frame++;
+    }
   }
   draw() {
-    ctx.fillRect(this.x, this.y, this.width, this.height);
+    ctx.drawImage(
+      this.image,
+      this.frame * this.spriteWidth,
+      0,
+      this.spriteWidth,
+      this.spriteHeight,
+      this.x,
+      this.y,
+      this.width,
+      this.height
+    );
   }
 }
 
-// const enemy1 = new Enemy();
-for (let i = 0; i < numberOfEnemies; i++) {}
+for (let i = 0; i < numberOfEnemies; i++) {
+  enemiesArray.push(new Enemy());
+}
 
 function animate() {
   ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
-  enemy1.update();
-  // Creates a rectangle with the enemy's parameters
-  enemy1.draw();
+  enemiesArray.forEach((enemy) => {
+    enemy.update();
+    enemy.draw();
+  });
+  gameFrame++;
   requestAnimationFrame(animate);
 }
 animate();
