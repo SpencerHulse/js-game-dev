f47ad53b76a1a63ba08ce2df15bebf890b258729
Spencer Hulse <hulse.spencer@gmail.com>
2022-08-08 02:21:03 -0400

    Created the skeleton for the enemy variety project.

---
+++ enemy-variety/script.js
@@ -0,0 +1,29 @@
+// Ensures all images are loaded before the animations begins
+document.addEventListener("load", () => {
+  const canvas = document.getElementById("canvas1");
+  const ctx = canvas.getContext("2d");
+  canvas.width = 500;
+  canvas.height = 800;
+
+  class Game {
+    constructor() {
+      this.enemies = [];
+    }
+    update() {}
+    draw() {}
+    // Hash means private, so it can only be called inside the class
+    #addNewEnemey() {}
+  }
+
+  class Enemy {
+    constructor() {}
+    update() {}
+    draw() {}
+  }
+
+  function animate() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    // All code for animation
+    requestAnimationFrame(animate);
+  }
+});
