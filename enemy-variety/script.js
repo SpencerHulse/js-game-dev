da2d99d9fca4694d90d9527320a45cd96915821c
Spencer Hulse <hulse.spencer@gmail.com>
2022-08-09 01:12:16 -0400

    Added logic to Game and Enemy classes to deal with adding enemies and updating the animation using deltaTime.

--- enemy-variety/script.js
+++ enemy-variety/script.js
@@ -1,29 +1,63 @@
 // Ensures all images are loaded before the animations begins
-document.addEventListener("load", () => {
+window.addEventListener("load", () => {
   const canvas = document.getElementById("canvas1");
   const ctx = canvas.getContext("2d");
   canvas.width = 500;
   canvas.height = 800;
 
   class Game {
-    constructor() {
+    constructor(ctx, width, height) {
+      this.ctx = ctx;
+      this.width = width;
+      this.height = height;
       this.enemies = [];
+      this.enemyInterval = 1000;
+      this.enemyTimer = 0;
     }
-    update() {}
-    draw() {}
+    update(deltaTime) {
+      if (this.enemyTimer > this.enemyInterval) {
+        this.#addNewEnemy();
+        this.enemyTimer = 0;
+      } else {
+        this.enemyTimer += deltaTime;
+      }
+      this.enemies.forEach((object) => object.update());
+    }
+    draw() {
+      this.enemies.forEach((object) => object.draw(this.ctx));
+    }
     // Hash means private, so it can only be called inside the class
-    #addNewEnemey() {}
+    #addNewEnemy() {
+      // Passing this passes everything from the class
+      this.enemies.push(new Enemy(this));
+    }
   }
 
   class Enemy {
-    constructor() {}
-    update() {}
-    draw() {}
+    constructor(game) {
+      this.game = game;
+      this.x = this.game.width;
+      this.y = Math.random() * this.game.height;
+      this.width = 100;
+      this.height = 100;
+    }
+    update() {
+      this.x--;
+    }
+    draw(ctx) {
+      ctx.fillRect(this.x, this.y, this.width, this.height);
+    }
   }
 
-  function animate() {
+  const game = new Game(ctx, canvas.width, canvas.height);
+  let lastTime = 1;
+  function animate(timeStamp) {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
-    // All code for animation
+    const deltaTime = timeStamp - lastTime;
+    lastTime = timeStamp;
+    game.update(deltaTime);
+    game.draw();
     requestAnimationFrame(animate);
   }
+  animate(0);
 });
