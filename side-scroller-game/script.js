6537e609ee199868c3b14ee310d98946f9d7bb09
Spencer Hulse <hulse.spencer@gmail.com>
2022-08-10 01:33:04 -0400

    Added the skeleton for a side scroller game.

---
+++ side-scroller-game/script.js
@@ -0,0 +1,27 @@
+window.addEventListener("load", () => {
+  const canvas = document.getElementById("canvas1");
+  const ctx = canvas.getContext("2d");
+  canvas.width = 800;
+  canvas.height = 720;
+
+  // Apply event listeners to keyboard events and hold an array of active keys
+  class InputHandler {}
+
+  // React to keys as they are pressed, drawing and updating the player
+  class Player {}
+
+  // Handles endlessly scrolling background
+  class Background {}
+
+  // Generates enemies
+  class Enemy {}
+
+  // Responsible for the multiple enemies in the game - adding, updating, and removing
+  function handleEnemies() {}
+
+  // Handles things like displaying score and game over message
+  function displayStatusText() {}
+
+  // Takes care of endlessly animating the game
+  function animate() {}
+});
