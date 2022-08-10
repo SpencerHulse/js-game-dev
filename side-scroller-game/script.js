9f88dd299963f94e846e69bffc8db2e79f2cb0a5
Spencer Hulse <hulse.spencer@gmail.com>
2022-08-10 19:43:12 -0400

    Created event listeners to handle keydown and keyup events for the side scroller game.

--- side-scroller-game/script.js
+++ side-scroller-game/script.js
@@ -5,7 +5,35 @@ window.addEventListener("load", () => {
   canvas.height = 720;
 
   // Apply event listeners to keyboard events and hold an array of active keys
-  class InputHandler {}
+  class InputHandler {
+    constructor() {
+      this.keys = [];
+      // To inherit this, must use an arrow function or bind method
+      window.addEventListener("keydown", (e) => {
+        // If it is that key and that key is not in the keys array, push it
+        if (
+          (e.key === "ArrowDown" ||
+            e.key === "ArrowUp" ||
+            e.key === "ArrowLeft" ||
+            e.key === "ArrowRight") &&
+          this.keys.indexOf(e.key) === -1
+        ) {
+          this.keys.push(e.key);
+        }
+      });
+      window.addEventListener("keyup", (e) => {
+        // Find and remove key from array when key released
+        if (
+          e.key === "ArrowDown" ||
+          e.key === "ArrowUp" ||
+          e.key === "ArrowLeft" ||
+          e.key === "ArrowRight"
+        ) {
+          this.keys.splice(this.keys.indexOf(e.key), 1);
+        }
+      });
+    }
+  }
 
   // React to keys as they are pressed, drawing and updating the player
   class Player {}
@@ -21,6 +49,8 @@ window.addEventListener("load", () => {
 
   // Handles things like displaying score and game over message
   function displayStatusText() {}
+
+  const input = new InputHandler();
 
   // Takes care of endlessly animating the game
   function animate() {}
