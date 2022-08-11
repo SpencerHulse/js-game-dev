ca5ef2e20da6c5dcfa10800cf24a9c56650b887b
Spencer Hulse <hulse.spencer@gmail.com>
2022-08-10 22:27:01 -0400

    Created player class with draw method to place it on canvas.

--- side-scroller-game/script.js
+++ side-scroller-game/script.js
@@ -36,7 +36,20 @@ window.addEventListener("load", () => {
   }
 
   // React to keys as they are pressed, drawing and updating the player
-  class Player {}
+  class Player {
+    constructor(gameWidth, gameHeight) {
+      this.gameWidth = gameWidth;
+      this.gameHeight = gameHeight;
+      this.width = 200;
+      this.height = 200;
+      this.x = 0;
+      this.y = this.gameHeight - this.height;
+    }
+    draw(context) {
+      context.fillStyle = "white";
+      context.fillRect(this.x, this.y, this.width, this.height);
+    }
+  }
 
   // Handles endlessly scrolling background
   class Background {}
@@ -51,6 +64,8 @@ window.addEventListener("load", () => {
   function displayStatusText() {}
 
   const input = new InputHandler();
+  const player = new Player(canvas.width, canvas.height);
+  player.draw(ctx);
 
   // Takes care of endlessly animating the game
   function animate() {}
