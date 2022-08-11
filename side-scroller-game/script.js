8c408722edd7bbd8faaf170a13dd1a381a56c97a
Spencer Hulse <hulse.spencer@gmail.com>
2022-08-11 03:41:15 -0400

    Added player jump (arrowUp) logic and onGround method.

--- side-scroller-game/script.js
+++ side-scroller-game/script.js
@@ -50,6 +50,8 @@ window.addEventListener("load", () => {
       this.frameX = 0;
       this.frameY = 0;
       this.speed = 0;
+      this.vy = 0;
+      this.weight = 1;
     }
     draw(context) {
       context.fillStyle = "white";
@@ -67,18 +69,36 @@ window.addEventListener("load", () => {
       );
     }
     update(input) {
-      // Horizontal movement
-      this.x += this.speed;
       if (input.keys.indexOf("ArrowRight") > -1) {
         this.speed = 5;
       } else if (input.keys.indexOf("ArrowLeft") > -1) {
         this.speed = -5;
+      } else if (input.keys.indexOf("ArrowUp") > -1 && this.onGround()) {
+        this.vy -= 32;
       } else {
         this.speed = 0;
       }
+      // Horizontal movement
+      this.x += this.speed;
       if (this.x < 0) this.x = 0;
       else if (this.x > this.gameWidth - this.width)
         this.x = this.gameWidth - this.width;
+      // Vertical Movement
+      this.y += this.vy;
+      if (!this.onGround()) {
+        this.vy += this.weight;
+        this.frameY = 1;
+      } else {
+        this.vy = 0;
+        this.frameY = 0;
+      }
+      // Boundary stops the potential of going through the floor
+      if (this.y > this.gameHeight - this.height)
+        this.y = this.gameHeight - this.height;
+    }
+    onGround() {
+      // Returns true or false for whether the player is on the ground
+      return this.y >= this.gameHeight - this.height;
     }
   }
 
