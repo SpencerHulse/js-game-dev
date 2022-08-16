54ff83d3a97133c8f098123303bbe00b61282224
Spencer Hulse <hulse.spencer@gmail.com>
2022-08-16 02:27:30 -0400

    Created project structure, Game class, and Player class.

---
+++ endless-runner-game/main.js
@@ -0,0 +1,33 @@
+import { Player } from "./classes/Player.js";
+
+window.addEventListener("load", () => {
+  const canvas = canvas1;
+  const ctx = canvas.getContext("2d");
+  canvas.width = 1500;
+  canvas.height = 500;
+
+  class Game {
+    constructor(width, height) {
+      this.width = width;
+      this.height = height;
+      this.player = new Player(this);
+    }
+    update() {
+      this.player.update();
+    }
+    draw() {
+      this.player.draw(ctx);
+    }
+  }
+
+  const game = new Game(canvas.width, canvas.height);
+  console.log(game);
+
+  function animate() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    game.update();
+    game.draw(ctx);
+    requestAnimationFrame(animate);
+  }
+  animate();
+});
