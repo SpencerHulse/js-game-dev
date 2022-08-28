5a3b9de40dca116b4bb3c0c90c6078046d163d54
Spencer Hulse <hulse.spencer@gmail.com>
2022-08-27 20:28:48 -0400

    Added basic climbing enemies.

--- endless-runner-game/classes/Enemies.js
+++ endless-runner-game/classes/Enemies.js
@@ -77,5 +77,19 @@ export class ClimbingEnemy extends Enemy {
   constructor(game) {
     super();
     this.game = game;
+    this.width = 120;
+    this.height = 144;
+    this.x = this.game.width;
+    this.y = Math.random() * this.game.height * 0.5;
+    this.image = enemy_spider;
+    this.speedX = 0;
+    this.speedY = Math.random() > 0.5 ? 1 : -1;
+    this.maxFrame = 5;
+  }
+  update(deltaTime) {
+    super.update(deltaTime);
+  }
+  draw(context) {
+    super.draw(context);
   }
 }
